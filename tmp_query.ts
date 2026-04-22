62d7305eb20877c224e512445c1b7ddde826b505
andikubusho <andikubusho@gmail.com>
2026-04-22 18:29:05 +0700

    fix: cleanup junk files causing build errors

--- tmp_query.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "./server/db";
-import { pelangganProgram } from "./shared/schema";
-
-async function run() {
-  const all = await db.select().from(pelangganProgram);
-  console.log("All pelangganProgram:", all);
-  process.exit(0);
-}
-run();
